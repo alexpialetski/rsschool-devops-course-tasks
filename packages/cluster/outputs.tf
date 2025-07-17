6da609ddd8c136a3234402ad1fffbbaf37c99a91
Aliaksei Pialetski <aliaksei.pialetski@workhuman.com>
2025-07-17 14:54:53 +0300

    new: kubeconfig as a secret

--- packages/cluster/outputs.tf
+++ packages/cluster/outputs.tf
@@ -1,1 +1,9 @@
+output "k3s_kubeconfig_secret_name" {
+  value       = aws_secretsmanager_secret.k3s_kubeconfig.name
+  description = "Secrets Manager secret name for k3s kubeconfig"
+}
 
+output "control_plane_instance_ids" {
+  value       = aws_instance.control_plane_node[*].id
+  description = "List of instance IDs for control plane nodes"
+}
